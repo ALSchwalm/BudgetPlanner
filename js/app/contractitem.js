c11bf8d35dc159d9392d6ed621045f3f90dd3b05
Adam Schwalm <adamschwalm@gmail.com>
2015-03-31 14:28:44 -0500

    Add basic item class to reduce redundancy

--- js/app/contractitem.js
+++ js/app/contractitem.js
@@ -2,7 +2,7 @@
  * A module which defines the 'rows' which are placed in the ContractWidget
  * @module app/contractitem
  */
-define(["jquery"], function(jquery){
+define(["jquery", "app/basicitem"], function(jquery, BasicItem){
     "use strict"
 
     /**
@@ -20,7 +20,7 @@ define(["jquery"], function(jquery){
         this.start = start;
         this.end = end;
         this.body = $("<div>")
-            .load("bodies/contractitem.html",
+            .load("bodies/basicitem.html",
                   null,
                   function(){
                       this.init();
@@ -32,117 +32,8 @@ define(["jquery"], function(jquery){
         return this;
     }
 
+    ContractItem.prototype = new BasicItem();
     ContractItem.prototype.itemName = "Contractual";
-
-    /**
-     * Initialize this item. This function will be invoked after the body
-     * div has been populated but before it is inserted into the DOM.
-     */
-    ContractItem.prototype.init = function() {
-        this.body.find('.item-remove').click(function(){
-            this.body.remove();
-            this.parentWidget.removeItem(this);
-        }.bind(this));
-
-        this.body.find('.contract-cost')
-            .keyup(this.update.bind(this));
-    }
-
-    /**
-     * Get an object which can be used to restore this item to a prior state
-     */
-    ContractItem.prototype.save = function() {
-        return {
-            name : this.body.find(".contract-name").val(),
-            cost : this.body.find(".contract-cost").val(),
-            year : this.body.find(".contract-year").val()
-        };
-    }
-
-    /**
-     * Restore this item to the state specified by 'config'
-     *
-     * @param {object} config - The configuration object
-     */
-    ContractItem.prototype.restore = function(config) {
-        this.start = this.parent.start;
-        this.end = this.parent.end;
-        this.updateDuration(this.start, this.end);
-        this.body.find(".contract-name").val(config.name);
-        this.body.find(".contract-cost").val(config.cost);
-        this.body.find(".contract-year").val(config.year);
-        this.update();
-    }
-
-    ContractItem.prototype.update = function() {
-    }
-
-    /**
-     * Make this item able to represent the duration start-end
-     *
-     * @param {Moment} start - New start time of the item
-     * @param {Moment} end - New end time of the item
-     */
-    ContractItem.prototype.updateDuration = function(start, end) {
-        this.start = start;
-        this.end = end;
-
-        var years = Math.ceil(end.diff(start, 'years', true));
-        while(this.body.find(".contract-year option").length != years) {
-            if (this.body.find(".contract-year option").length > years) {
-                this.removeYear();
-            } else {
-                this.addYear();
-            }
-        }
-    }
-
-
-    /**
-     * Add a year to this item's display
-     */
-    ContractItem.prototype.addYear = function() {
-        var year = this.body.find(".contract-year option").length;
-        this.body.find(".contract-year").append(
-            $("<option>").attr("value", year).text("Year " + (year+1))
-        );
-    }
-
-    /**
-     * Remove a year from this item's display
-     */
-    ContractItem.prototype.removeYear = function() {
-        this.body.find(".contract-year option:last").remove();
-    }
-
-    /**
-     * Get the current costs for each year from this item.
-     *
-     * @returns {Number[]} - An array of numbers, one for each year
-     */
-    ContractItem.prototype.val = function() {
-        var arr = [];
-        for (var i=0; i < this.body.find("option").length; ++i) {
-            arr.push(0);
-        }
-        var purchaseYear = parseInt(this.body.find(".contract-year").val());
-        arr[purchaseYear] = parseFloat(this.body.find('.contract-cost').val()) || 0;
-        return arr;
-    }
-
-    /**
-     * Convert this item to an array suitable for being passed to excel-builder
-     */
-    ContractItem.prototype.serialize = function() {
-        var name = this.body.find(".contract-name").val();
-        var cost = this.body.find(".contract-cost").val();
-        var year = this.body.find(".contract-year").text();
-        return [
-            name, cost, year,
-            {value: 'INDIRECT("B" & ROW())',
-             metadata: {type: 'formula'}}
-        ];
-    }
 
     return ContractItem;
 });
